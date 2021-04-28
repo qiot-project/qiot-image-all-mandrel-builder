e35cc5f317218ee5f3a415bde57ac4b18cc9cada
Geoff Newson <geoff.newson@gmail.com>
2021-04-28 22:40:32 +0100

    Adding dockerfile and python script to extract the versions for tagging

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # qiot-image-all-mandrel-builder
+
+This repo contains the docker file to build the mandrel builder image and
+tooling to build and push it to quay.io on any changes.
---
+++ mandrel_image_versions.py
@@ -0,0 +1,27 @@
+import re
+
+def main():
+    mav_re = re.compile('MAVEN_VERSION=([\d\.]*)')
+    jav_re = re.compile('JAVA_PACKAGE=.*?([\d]+)')
+    man_re = re.compile('MANDREL_VERSION=.*?([\d\.]+)')
+
+    mav_vers = ""
+    jav_vers = ""
+    man_vers = ""
+
+    with open("maven-jdk-mandrel-builder.Dockerfile", "r") as f:
+        for line in f:
+            if result := mav_re.search(line):
+                mav_vers = result.group(1)
+            elif result := jav_re.search(line):
+                jav_vers = result.group(1)
+            elif result := man_re.search(line):
+                man_vers = result.group(1)
+
+    tag = f'{mav_vers}-{jav_vers}-{man_vers}'
+    print(tag)
+
+    return tag
+
+if __name__ == "__main__":
+    main()
